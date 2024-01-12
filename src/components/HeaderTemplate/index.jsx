b595d1e572447d8a353bf3f0a0a9717cce48b8f1
Claudia Lim <climweiching@gmail.com>
2024-01-12 11:48:04 +0000

    add placeholder header background

--- src/components/HeaderTemplate/index.jsx
+++ src/components/HeaderTemplate/index.jsx
@@ -1,9 +1,9 @@
 function HeaderTemplate () {
     return (
-        <div>
+        <div className="header">
+            {/*<img className="header-image" alt="header image" src="src/assets/milad-fakurian-u8Jn2rzYIps-unsplash.jpg"></img>*/}
             <h1>Hi, I'm Claudia</h1>
             <h2>GP turned Software Developer</h2>
-
         </div>
     )
 }
