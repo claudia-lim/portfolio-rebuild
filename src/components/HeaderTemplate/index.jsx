3db8134ebdd5a5495e833f7fce1a665862df8feb
Claudia Lim <climweiching@gmail.com>
2024-01-13 12:19:06 +0000

    header styling

--- src/components/HeaderTemplate/index.jsx
+++ src/components/HeaderTemplate/index.jsx
@@ -1,11 +1,11 @@
 import './header.css'
+import {useState} from "react";
 function HeaderTemplate () {
 
-
     return (
-        <div className="header" >
-            <h1 className="hidden">Hi, I'm Claudia</h1>
-            <h2 >GP turned Software Developer</h2>
+        <div className="header">
+            <h1>Hi, I'm Claudia</h1>
+            <h2>GP turned Software Developer</h2>
         </div>
     )
 }
