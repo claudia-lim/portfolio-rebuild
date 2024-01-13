2243908b049f7aa8a83a89c3d1735542062e63e8
Claudia Lim <climweiching@gmail.com>
2024-01-13 11:19:44 +0000

    font

--- src/components/HeaderTemplate/index.jsx
+++ src/components/HeaderTemplate/index.jsx
@@ -1,10 +1,11 @@
 import './header.css'
 function HeaderTemplate () {
+
+
     return (
-        <div className="header">
-            {/*<img className="header-image" alt="header image" src="src/assets/milad-fakurian-u8Jn2rzYIps-unsplash.jpg"></img>*/}
-            <h1>Hi, I'm Claudia</h1>
-            <h2>GP turned Software Developer</h2>
+        <div className="header" >
+            <h1 className="hidden">Hi, I'm Claudia</h1>
+            <h2 >GP turned Software Developer</h2>
         </div>
     )
 }
