3adcaf590ee47fc336d9d1e1632355f201b47543
Claudia Lim <climweiching@gmail.com>
2024-01-15 15:23:31 +0000

    update skills section

--- src/components/SkillsTemplate/index.jsx
+++ src/components/SkillsTemplate/index.jsx
@@ -1,6 +1,6 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCss3Alt, faGit, faHtml5, faJs, faNodeJs, faPhp, faReact} from "@fortawesome/free-brands-svg-icons";
-import {faDatabase} from "@fortawesome/free-solid-svg-icons";
+import {faDatabase, faDiagramProject, faT} from "@fortawesome/free-solid-svg-icons";
 import SkillsAtom from "../SkillsAtom/index.jsx";
 import './skills.css';
 
@@ -11,6 +11,7 @@ function SkillsTemplate () {
             <h3 className="skills-title">Skills Section</h3>
             <div id="skills">
                 <SkillsAtom icon={<FontAwesomeIcon icon={faPhp} />} skillName="PHP" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=php&sort="/>
+                <SkillsAtom icon={<FontAwesomeIcon icon={faT} />} skillName="Typescript" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=typescript&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faJs}/>}  skillName="Javascript" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=javascript&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faReact} />} skillName="React" link="https://github.com/claudia-lim?tab=repositories&q=react&type=&language=&sort=" />
                 <SkillsAtom icon={<FontAwesomeIcon icon={faNodeJs} />} skillName="NodeJS" link="https://github.com/claudia-lim?tab=repositories&q=node&type=&language=&sort="/>
@@ -19,6 +20,7 @@ function SkillsTemplate () {
                 <SkillsAtom icon={<FontAwesomeIcon icon={faDatabase} />} skillName="MySQL" link="https://github.com/claudia-lim?tab=repositories&q=mysql&type=&language=&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faDatabase} />} skillName="MongoDB" link="https://github.com/claudia-lim?tab=repositories&q=mongodb&type=&language=&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faGit} />} skillName="Git"/>
+                <SkillsAtom icon={<FontAwesomeIcon icon={faDiagramProject} />} skillName="Agile" />
             </div>
         </div>
     )
