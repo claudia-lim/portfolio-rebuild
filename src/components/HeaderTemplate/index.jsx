91001c8899904474bdce4e00ca3cfc605a92ec61
Claudia Lim <climweiching@gmail.com>
2024-01-15 14:42:36 +0000

    header update

--- src/components/HeaderTemplate/index.jsx
+++ src/components/HeaderTemplate/index.jsx
@@ -6,7 +6,7 @@ function HeaderTemplate () {
         <div className="header">
             <img src="../src/assets/profile-round.png"></img>
             <div className="header-text">
-                <h1>Hi, I'm Claudia</h1>
+                <h1>Hi, I'm Claudia!</h1>
                 <h3>Doctor turned Software Developer</h3>
                 <p>Navigating the realms of full-stack development to merge creativity, functionality, and technology seamlessly.</p>
             </div>
