9898d2ff364b69e20180959ae352f545da415bfa
Claudia Lim <climweiching@gmail.com>
2024-01-15 16:10:30 +0000

    tidy up header and about section

--- src/components/AboutTemplate/index.jsx
+++ src/components/AboutTemplate/index.jsx
@@ -6,11 +6,12 @@ function AboutTemplate () {
             <p>
                 After nearly 10 years as a doctor, while on the hunt for a new challenge,
                 I discovered a passion for coding and software development.
-                I hope to use my previously honed skills in <span className="emphasis">problem solving, collaboration, communication and data interpretation</span>
-                to bring a unique perspective to the tech industry.
+                I hope to use my previously honed skills in <span className="emphasis">problem solving, collaboration, communication and data interpretation</span> to bring a unique perspective to the tech industry.
                 After embarking on an intensive 4 month coding bootcamp at <span className="emphasis"><a href="https://io-academy.uk/">iO Academy</a></span> in Bath, UK,
                 I am well versed in the use of <span className="emphasis">PHP, HTML, CSS, Javascript/Typescript</span> for full stack development.
-                Please have a look through some of my projects and get in contact if you'd like to connect!
+            </p>
+            <p>
+                Please have a look through some of my projects to see examples of my work. Do get in contact if you'd like to connect!
             </p>
         </div>
     )
