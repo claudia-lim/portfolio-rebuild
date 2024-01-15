d5b84c793befb3fae622150b8e5a4e7c38c38374
Claudia Lim <climweiching@gmail.com>
2024-01-15 17:57:07 +0000

    project carousel experimental set up

--- src/components/ProjectMolecule/index.jsx
+++ src/components/ProjectMolecule/index.jsx
@@ -1,7 +1,10 @@
-function ProjectMolecule () {
+function ProjectMolecule ({src, alt, name, description, liveLink}) {
     return (
-        <>
-        </>
+        <div className="project">
+            <h4>{name}</h4>
+            <a href={liveLink}><img src={src} alt={alt}></img></a>
+            <p>{description}</p>
+        </div>
     )
 }
 
--- src/components/ProjectsTemplate/index.jsx
+++ src/components/ProjectsTemplate/index.jsx
@@ -1,7 +1,26 @@
+import "./projectstemplate.css";
+import {projects} from "../../data/projects.json"
+import ProjectMolecule from "../ProjectMolecule/index.jsx";
+
 function ProjectsTemplate() {
+// const projects = [{name: "test", src: ""}, {name: "test2", src: ""}]
+
+    console.log(projects);
     return (
         <div id="projects">
             <h3>Projects Section</h3>
+            <div className="experiment-scroll">
+                {projects.map((project, index) => {
+                    return <ProjectMolecule
+                        src={project.src}
+                        alt={project.alt}
+                        name={project.name}
+                        description={project.description}
+                        liveLink={project.liveLink}
+                        githubLink={project.githubLink}
+                        key={index}/>
+                })}
+            </div>
         </div>
     )
 }
