931a7a3c6b41b52e4441ec5c8ae0bbbe6fc5b967
Claudia Lim <climweiching@gmail.com>
2024-01-16 16:52:42 +0000

    update project information

--- src/components/ProjectMolecule/index.jsx
+++ src/components/ProjectMolecule/index.jsx
@@ -1,9 +1,10 @@
+import "./projectmolecule.css"
 function ProjectMolecule ({src, alt, name, description, liveLink}) {
     return (
         <div className="project">
             <h4>{name}</h4>
-            <a href={liveLink}><img src={src} alt={alt}></img></a>
-            <p>{description}</p>
+            <a href={liveLink} target="_blank"><img src={src} alt={alt}></img></a>
+            {/*<p>{description}</p>*/}
         </div>
     )
 }
