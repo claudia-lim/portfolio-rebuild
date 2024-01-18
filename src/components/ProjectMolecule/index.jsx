bd896713fd16e0aca54978614e3f03f3377c5f11
Claudia Lim <climweiching@gmail.com>
2024-01-18 16:51:53 +0000

    carousel for projects using swiper library

--- src/components/ProjectMolecule/index.jsx
+++ src/components/ProjectMolecule/index.jsx
@@ -1,10 +1,23 @@
 import "./projectmolecule.css"
-function ProjectMolecule ({src, alt, name, description, liveLink}) {
+import {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircle} from "@fortawesome/free-solid-svg-icons";
+function ProjectMolecule ({src, alt, name, description, liveLink, index, currentProj, projects}) {
+
     return (
-        <div className="project">
+        <div id={`project-${index}`} className={(currentProj === index) ? "project-slide" : "project-hidden"}>
             <h4>{name}</h4>
-            <a href={liveLink} target="_blank"><img src={src} alt={alt}></img></a>
-            {/*<p>{description}</p>*/}
+            <div className="project-card" >
+                <div className="project-img">
+                    <a href={liveLink} target="_blank">
+                        <img src={src} alt={alt}></img>
+                    </a>
+                </div>
+                <div className="project-text">
+                    <p>{description}</p>
+                    <p className="proj-links"></p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ProjectsTemplate/index.jsx
+++ src/components/ProjectsTemplate/index.jsx
@@ -1,28 +1,105 @@
 import "./projectstemplate.css";
 import {projects} from "../../data/projects.json"
 import ProjectMolecule from "../ProjectMolecule/index.jsx";
+import {useRef, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCaretLeft, faCaretRight, faCircle} from "@fortawesome/free-solid-svg-icons";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './styles.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
 
 function ProjectsTemplate() {
-// const projects = [{name: "test", src: ""}, {name: "test2", src: ""}]
 
-    console.log(projects);
+const [currentProj, setCurrentProj] = useState(0);
+
+    function clickLeft () {
+    if (currentProj === 0) {
+        setCurrentProj(projects.length - 1);
+    } else {
+        setCurrentProj(currentProj - 1);
+    }
+}
+
+function clickRight () {
+    if (currentProj === projects.length - 1) {
+        setCurrentProj(0);
+    } else {
+        setCurrentProj(currentProj + 1);
+    }
+}
     return (
+        <>
         <div id="projects">
             <h3>Projects Section</h3>
-            <div className="experiment-scroll">
-                {projects.map((project, index) => {
-                    return <ProjectMolecule
-                        src={project.src}
-                        alt={project.alt}
-                        name={project.name}
-                        description={project.description}
-                        liveLink={project.liveLink}
-                        githubLink={project.githubLink}
-                        key={index}/>
-                })}
-            </div>
+        {/*    <div className="carousel">*/}
+
+        {/*        <button onClick={clickLeft} className="carousel-button button-left">*/}
+        {/*            <FontAwesomeIcon icon={faCaretLeft}/>*/}
+        {/*        </button>*/}
+
+        {/*        {projects.map((project, index) => {*/}
+        {/*            return <ProjectMolecule*/}
+        {/*                src={project.src}*/}
+        {/*                alt={project.alt}*/}
+        {/*                name={project.name}*/}
+        {/*                description={project.description}*/}
+        {/*                liveLink={project.liveLink}*/}
+        {/*                githubLink={project.githubLink}*/}
+        {/*                index={index}*/}
+        {/*                currentProj={currentProj}*/}
+        {/*                key={index}*/}
+        {/*                projects={projects.length}*/}
+        {/*            />*/}
+        {/*        })}*/}
+
+        {/*        <button onClick={clickRight} className="carousel-button button-right">*/}
+        {/*            <FontAwesomeIcon icon={faCaretRight}/>*/}
+        {/*        </button>*/}
+        {/*    </div>*/}
+        {/*    <div className="indicator-bar">*/}
+        {/*        {projects.map((project, index) => {*/}
+        {/*            return <div className={(currentProj === index) ? "indicator-highlighted" : ""}*/}
+        {/*                        key={index}*/}
+        {/*                        id={`indicator-${index}`}>*/}
+        {/*                        <FontAwesomeIcon icon={faCircle}/>*/}
+        {/*            </div>*/}
+        {/*        })}*/}
+        {/*    </div>*/}
+        {/*</div>*/}
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    loop={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    {projects.map((project, index) => {
+                        return <SwiperSlide key={index}>
+                            <div className="project-card-slider">
+                                <h4>{project.name}</h4>
+                                <a href={project.liveLink}>
+                                <img src={project.src}></img></a>
+                                <p>{project.description}</p>
+                            </div>
+                        </SwiperSlide>
+                    })}
+                </Swiper>
         </div>
-    )
+    </>
+)
 }
 
 export default ProjectsTemplate
