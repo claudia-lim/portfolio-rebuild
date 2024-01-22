710f52278731e0d4d400b8973dbc414f9a146538
Claudia Lim <climweiching@gmail.com>
2024-01-22 14:53:40 +0000

    logo and colour scheme

--- src/components/LogoAtom/index.jsx
+++ src/components/LogoAtom/index.jsx
@@ -1,11 +1,14 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faC, faL} from "@fortawesome/free-solid-svg-icons";
+import "./logo.css"
 
 function LogoAtom () {
     return (
         <div className="logo">
-            <FontAwesomeIcon icon={faC} />
-            <FontAwesomeIcon icon={faL} />
+            {/*<h5>CL</h5>*/}
+            &#160;Claudia Lim
+            {/*<FontAwesomeIcon icon={faC} />*/}
+            {/*<FontAwesomeIcon icon={faL} />*/}
         </div>
     )
 }
