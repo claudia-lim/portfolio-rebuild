b458a061d6c2ab085309494fd3b7c15dbf484b54
Claudia Lim <climweiching@gmail.com>
2024-05-03 15:21:12 +0100

    ammend skills

--- src/components/SkillsTemplate/index.jsx
+++ src/components/SkillsTemplate/index.jsx
@@ -14,11 +14,9 @@ function SkillsTemplate () {
                 <SkillsAtom icon={<FontAwesomeIcon icon={faT} />} skillName="Typescript" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=typescript&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faJs}/>}  skillName="Javascript" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=javascript&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faReact} />} skillName="React" link="https://github.com/claudia-lim?tab=repositories&q=react&type=&language=&sort=" />
-                <SkillsAtom icon={<FontAwesomeIcon icon={faNodeJs} />} skillName="NodeJS" link="https://github.com/claudia-lim?tab=repositories&q=node&type=&language=&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faHtml5} />} skillName="HTML" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=html&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faCss3Alt} />} skillName="CSS" link="https://github.com/claudia-lim?tab=repositories&q=&type=&language=css&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faDatabase} />} skillName="MySQL" link="https://github.com/claudia-lim?tab=repositories&q=mysql&type=&language=&sort="/>
-                <SkillsAtom icon={<FontAwesomeIcon icon={faDatabase} />} skillName="MongoDB" link="https://github.com/claudia-lim?tab=repositories&q=mongodb&type=&language=&sort="/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faGit} />} skillName="Git"/>
                 <SkillsAtom icon={<FontAwesomeIcon icon={faDiagramProject} />} skillName="Agile" />
             </div>
